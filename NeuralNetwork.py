0426e25d2832b7be1f22d95ef227209188b75146
Pacific <mm.kohani93@gmail.com>
2019-05-02 20:16:40 +0430

    nn

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -10,3 +10,7 @@ class ValueEstimator:
         for i in range(n_layers-2):
             self.model.add(Dense(hidden_dim, input_dim=hidden_dim, activation='relu'))
         self.model.add(Dense(1, activation='tanh'))
+
+
+    def make_prediction(self,input, player_turn):
+        self.model.
