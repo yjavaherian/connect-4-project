20fc28287b74faa908e83f6099198e4763792db0
Pacific <mm.kohani93@gmail.com>
2019-05-02 20:09:46 +0430

    nn

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,12 +2,11 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 class ValueEstimator:
-    def __init__(self,n_layers,input_dim):
+    def __init__(self,n_layers,input_dim,hidden_dim):
         self.n_layers=n_layers
         self.input_dim=input_dim
         self.model = Sequential()
-        for i in range(n_layers):
-            self.model.add(Dense(, input_dim=8, activation='relu'))
-
-        self.model.add(Dense(8, activation='relu'))
-        self.model.add(Dense(1, activation='sigmoid'))
+        self.model.add(Dense(hidden_dim, input_dim=input_dim, activation='relu'))
+        for i in range(n_layers-2):
+            self.model.add(Dense(hidden_dim, input_dim=hidden_dim, activation='relu'))
+        self.model.add(Dense(1, activation='tanh'))
